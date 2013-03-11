307c594e0fa0d06483dfc4b8bfc5c3c9ef90f9f2
Richard Hajdu <tuskone16@gmail.com>
2013-03-11 12:14:30 +0100

    debugmode fixed.

--- modules/debugmode/debugmode.js
+++ modules/debugmode/debugmode.js
@@ -1,5 +1,3 @@
-/* Sets debugMode */
-var debugMode = true;
 /* Creates HTML elements to show abd displays them */
 $("#daGame").after('<div class="push"></div>');
 $(".wrapper").after('<div class="debug"><div class="debuginfo"></div></div>');
